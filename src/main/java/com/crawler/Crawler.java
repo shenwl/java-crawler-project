c10228cb07440611253eae8096ab73be42859de6
shenwl <shenweilun96@gmail.com>
2019-12-03 23:38:18 +0800

    crawler和数据库操作逻辑分离

---
+++ src/main/java/com/crawler/Crawler.java
@@ -0,0 +1,118 @@
+package com.crawler;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Crawler {
+    DataAccess dao = new DataAccess();
+
+    public static void main(String[] args) {
+        try {
+            new Crawler().run();
+        } catch (IOException e) {
+            System.err.println("爬虫启动失败：" + e.getMessage());
+        }
+    }
+
+    public void run() throws IOException {
+        String link = null;
+
+        while ((link = dao.getNextLink()) != null) {
+            if (dao.linkHasProcessed(link) || !isSinaNewsLink(link)) {
+                continue;
+            }
+
+            Document doc = requestAndParseHtml(link);
+
+            parseLinksFromPageAndStoreIntoDatabase(doc);
+
+            parseNewsFromPageAndStoreInfoDatabase(doc, link);
+
+            // 从待处理link池取出，放入已处理池
+            dao.processLink(link);
+        }
+
+    }
+
+    private void parseNewsFromPageAndStoreInfoDatabase(Document doc, String link) {
+        ArrayList<Element> articles = doc.select("article");
+
+        if (!articles.isEmpty()) {
+            for (Element article : articles) {
+                News news = getNewsFromArticleEl(article, link);
+                dao.insertNews(news);
+            }
+        }
+    }
+
+    private News getNewsFromArticleEl(Element article, String link) {
+        String title = article.select(".art_tit_h1").get(0).text();
+        ArrayList<Element> paragraphs = article.select("p");
+
+        String content = paragraphs.stream().map(Element::text).collect(Collectors.joining("\n"));
+
+        return News.createNews(link, title, content);
+    }
+
+    private void parseLinksFromPageAndStoreIntoDatabase(Document doc) {
+        List<String> links = getLinksFromDoc(doc);
+
+        for (String link : links) {
+            dao.insertLink("insert into LINKS_TO_BE_PROCESSED (link) values (?)", link);
+        }
+    }
+
+
+    private static boolean isSinaNewsLink(String link) {
+        if (link.contains("passport.sina.cn")) {
+            return false;
+        }
+        return link.contains("news.sina.cn") || "https://sina.cn".equals(link);
+    }
+
+    private static ArrayList<String> getLinksFromDoc(Document doc) {
+        ArrayList<Element> linkTags = doc.select("a");
+        ArrayList<String> links = new ArrayList<>();
+
+        linkTags.stream().map(linkTag -> linkTag.attr("href"))
+                .filter(Crawler::isSinaNewsLink)
+                .forEach(links::add);
+
+        return links;
+    }
+
+    private static void setCrawlerHeader(HttpRequestBase request) {
+        request.setHeader(
+                "User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"
+        );
+    }
+
+
+    private static Document requestAndParseHtml(String link) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        setCrawlerHeader(httpGet);
+        try (CloseableHttpResponse res = httpClient.execute(httpGet)) {
+            System.out.println(res.getStatusLine());
+            System.out.println(link);
+
+            HttpEntity entity = res.getEntity();
+            String html = EntityUtils.toString(entity);
+            return Jsoup.parse(html);
+        }
+    }
+}
---
+++ src/main/java/com/crawler/DataAccess.java
@@ -0,0 +1,99 @@
+package com.crawler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataAccess {
+    Connection connection;
+
+    public DataAccess() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:h2:file:/Users/shenwl/Projects/java-crawler-project/news");
+        } catch (SQLException e) {
+            System.err.println("数据库连接失败：" + e.getMessage());
+        }
+    }
+
+    private ArrayList<String> loadUrls(String sql) {
+        ArrayList<String> urls = new ArrayList<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                urls.add(resultSet.getString(1));
+            }
+        } catch (SQLException e) {
+            System.err.println("loadUrls failed" + e.getMessage());
+        }
+        return urls;
+    }
+
+    public String getNextLink() {
+        List<String> linkPool = loadUrls("select link from LINKS_TO_BE_PROCESSED limit 1");
+        if (linkPool.isEmpty()) {
+            return null;
+        }
+        return linkPool.get(0);
+    }
+
+
+    public void insertLink(String sql, String link) {
+        try {
+            PreparedStatement state = connection.prepareStatement(sql);
+            state.setString(1, link);
+            state.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("insertLink failed" + e.getMessage());
+        }
+    }
+
+    public boolean linkHasProcessed(String link) {
+        boolean processed = false;
+
+        try {
+            PreparedStatement state = connection.prepareStatement("select link from LINKS_ALREADY_PROCESSED where link = ?");
+            state.setString(1, link);
+            ResultSet resultSet = state.executeQuery();
+
+            while (resultSet.next()) {
+                processed = true;
+            }
+        } catch (SQLException e) {
+            System.err.println("linkHasProcessed failed" + e.getMessage());
+        }
+
+        return processed;
+    }
+
+    public void processLink(String link) {
+        try {
+            PreparedStatement statement1 = connection.prepareStatement("DELETE FROM LINKS_TO_BE_PROCESSED where link = ?");
+            statement1.setString(1, link);
+            statement1.executeUpdate();
+
+            insertLink("insert into LINKS_ALREADY_PROCESSED (link) values (?)", link);
+        } catch (SQLException e) {
+            System.err.println("processLink failed" + e.getMessage());
+        }
+    }
+
+    public void insertNews(News news) {
+        try {
+            PreparedStatement state = connection.prepareStatement("insert into NEWS (URL, TITLE, CONTENT, CREATED_AT, MODIFIED_AT) values (?, ?, ?, ?, ?)");
+            state.setString(1, news.getUrl());
+            state.setString(2, news.getTitle());
+            state.setString(3, news.getContent());
+            state.setTimestamp(4, news.getCreatedAt());
+            state.setTimestamp(5, news.getModifiedAt());
+            state.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("insertNews failed" + e.getMessage());
+        }
+    }
+}
