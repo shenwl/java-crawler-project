4b7432c9a19cd08813b51c649246c140d40aa8fa
shenwl <shenweilun96@gmail.com>
2019-12-03 19:58:56 +0800

    使用flyway做migrate

---
+++ src/main/resources/db/migration/V1__Create_table.sql
@@ -0,0 +1,16 @@
+create table news (
+    id  bigint primary key auto_increment,
+    title text,
+    content text,
+    url varchar(500),
+    created_at timestamp,
+    modified_at timestamp,
+);
+
+create table links_to_be_processed (
+    link varchar(500),
+);
+
+create table links_already_processed (
+    link varchar(500),
+);
