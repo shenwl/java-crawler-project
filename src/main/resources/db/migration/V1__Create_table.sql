91de015716200e57f0a72624d0276afde7c2e46f
shenwl <shenweilun96@gmail.com>
2019-12-05 18:53:05 +0800

    使用mysql，migration sql语句兼容mysql

--- src/main/resources/db/migration/V1__Create_table.sql
+++ src/main/resources/db/migration/V1__Create_table.sql
@@ -3,14 +3,14 @@ create table news (
     title text,
     content text,
     url varchar(500),
-    created_at timestamp,
-    modified_at timestamp,
+    created_at timestamp default now(),
+    modified_at timestamp default now()
 );
 
 create table links_to_be_processed (
-    link varchar(500),
+    link varchar(500)
 );
 
 create table links_already_processed (
-    link varchar(500),
+    link varchar(500)
 );
